e4982d2d184030c865755467960f5e81c7dc4f86
Brice Shatzer <brice.shatzer@gmail.com>
2020-05-19 04:42:45 -0500

    added reset button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import SignupForm from './SignupForm';
+import reset from './reset.svg';
 
 export let baseUserValues = {
   'name' : {
@@ -39,28 +40,40 @@ class App extends React.Component {
   render(){
     
     return (
-      <div className="App">
-        {this.state.userValueIsSubmitted ? 
-          <Confirmation 
-            firstName={this.state.userValues.name.value} 
-            email={this.state.userValues.emailAddress.value} 
-          /> :
-          <SignupForm 
-            updateAppState={this.submitUserValue} 
-          />
-        }
-      </div>
+      <React.Fragment>
+        <div className="App">
+          {this.state.userValueIsSubmitted ? 
+            <Confirmation 
+              firstName={this.state.userValues.name.value} 
+              email={this.state.userValues.emailAddress.value} 
+            /> :
+            <SignupForm 
+              updateAppState={this.submitUserValue} 
+            />
+          }
+        </div>
+        {this.state.userValueIsSubmitted && 
+        <img 
+          src={reset}
+          className="resetLogo"
+          alt="resetLogo"
+          onClick={()=>this.setState({
+            userValueIsSubmitted: false,
+            userValues: {...baseUserValues}
+          })} 
+        />}
+      </React.Fragment>
     )
   }
 }
 
 
 function Confirmation (props) {return (
-  <React.Fragment>    
-    <h3>
+  <React.Fragment>
+    <h1>
       Welcome,<br />
-      <strong>{props.firstName}</strong>
-    </h3>
+      <strong>{props.firstName}!</strong>
+    </h1>
     <p>
       You have been registered for this awesome service.<br/>
       Please check your email listed below for instructions. 
@@ -69,5 +82,7 @@ function Confirmation (props) {return (
     <button>Sign In</button>
   </React.Fragment>
 )}
+
+
 
 export default App;
