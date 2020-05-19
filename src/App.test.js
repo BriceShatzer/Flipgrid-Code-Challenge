a8e7c7bae89f088395d0fa8c3521581970c577a0
Brice Shatzer <brice.shatzer@gmail.com>
2020-05-19 02:21:39 -0500

    segment out SignupForm tests from App tests

--- src/App.test.js
+++ src/App.test.js
@@ -9,161 +9,52 @@ let getElementById = (id, container) => {
   return queryByAttribute('id', container, id);
 }
 
-describe('Input Fields', () => {
-  function checkInitialState (inputEl) {
-    expect(inputEl.tagName).toEqual('INPUT');
-    expect(inputEl.className).toEqual('untouched');
-  }
-  
-  beforeEach(() => {
-    app = render(<App />);
-  });
+const testName = 'Parker';
+const testEmailAddress = 'abc@def123.com';
+const testPassword = 'aBcEf1234%$&*';
 
-  test('name input field ', () => {
-    const nameInput = getElementById('name', app.container);
-
-    // 'name' -- initial state 
-    expect(nameInput.type).toEqual('text');
-    checkInitialState(nameInput);
-
-    // -- onChange to valid
-    fireEvent.change(nameInput, { target: { value: 'Parker' } })
-    expect(nameInput.className).toEqual('valid');
-
-    // -- onChange to invalid
-    fireEvent.change(nameInput, { target: { value: '' } })
-    expect(nameInput.className).toEqual('inValid');
-
-  }),
-
-  test('emailAddress input field ', () => {
-    const emailAddressInput = getElementById('emailAddress', app.container);
-
-    // 'emailAddress' -- initial state 
-    expect(emailAddressInput.type).toEqual('text');
-    checkInitialState(emailAddressInput);
-    
-    // -- onChange to valid
-    fireEvent.change(emailAddressInput, { target: { value: 'abc@def123.com' } });
-    expect(emailAddressInput.className).toEqual('valid'); 
-
-    // -- onChange to invalid
-    fireEvent.change(emailAddressInput, { target: { value: '' } });
-    expect(emailAddressInput.className).toEqual('inValid');
-  
-    fireEvent.change(emailAddressInput, { target: { value: 'abcdef123com' } });
-    expect(emailAddressInput.className).toEqual('inValid');
-
-  }),
-
-  test('password input field', () => {
-    const passwordInput = getElementById('password', app.container);
-    
-    // 'password' ---  initial state
-    expect(passwordInput.type).toEqual('password');
-    checkInitialState(passwordInput);
-    
-    // -- onChange to valid
-    fireEvent.change(passwordInput, { target: { value: 'aBcEf1234%$&*' } });
-    expect(passwordInput.className).toEqual('valid'); 
-    
-    // -- onChange to invalid
-    fireEvent.change(passwordInput, { target: { value: '' } });
-    expect(passwordInput.className).toEqual('inValid');
-  })
-});
-
-describe('Sign Up Button', () => {
-  test('is disabled when fields are invalid',()=>{
-    app = render(<App />);
-    const submitButton = getByText(app.container, 'Sign Up');
-    expect(submitButton.disabled).toBeTruthy();
-  }),
-  test('is enabled when fields are valid',()=>{
+describe('App views', () => {
+  test('Confirmation view displayed after ',()=>{
     app = render(<App />);
     const nameInput = getElementById('name', app.container);
     const emailInput = getElementById('emailAddress', app.container);
     const passwordInput = getElementById('password', app.container);
-    const submitButton = getByText(app.container, 'Sign Up');
-
+    
     fireEvent.change(
       nameInput,
-      { target: { value: 'Parker' } }
+      { target: { value: testName} }
     );
     fireEvent.change(
       emailInput, 
-      { target: { value: 'abc@def123.com' } }
+      { target: { value: testEmailAddress } }
     );
     fireEvent.change(
       passwordInput,
-      { target: { value: 'aBcEf1234%$&*' } }
+      { target: { value: testPassword} }
     );
-    expect(submitButton.disabled).toBeFalsy();
+    fireEvent.click(
+      getByText(app.container, 'Sign Up')
+    )
+    
+    expect(getByText(app.container, testName))
+      .toMatchInlineSnapshot(`
+        <strong>
+          ${testName}
+        </strong>
+      `);
+
+    expect(getByText(app.container, testEmailAddress))
+      .toMatchInlineSnapshot(`
+        <p>
+          ${testEmailAddress}
+        </p>
+      `);
+
+    expect(getByText(app.container, 'Sign In'))
+      .toMatchInlineSnapshot(`
+        <button>
+          Sign In
+        </button>
+      `);
   })
 });
-
-test('tests are a thing', () => {
-  expect( typeof 'string' === 'string').toEqual(true);
-});
-
-
-// getByLabelText(container, 'Username')
-// console.log(getElementById('name').value);
-  // test('alt', ()=>{
-  //   const {getByLabelText} = render(<App />);
-  //   const el = getByLabelText('Email Address')    
-  //   console.log(el);
-    // const {container, getByLabelText} = render(<App />);
-    // const yo = render(App.call(this, 
-    //   submitUserValue({
-    //   userValueIsSubmitted: true,
-    //   userValues: {  
-    //     'name' : {
-    //       value: 'john',
-    //       isValid: true
-    //     }, 
-    //     'emailAddress' : {
-    //       value: 'abc@123.org',
-    //       isValid: true,
-    //     },
-    //     'password' : {
-    //       value: 'abcdef',
-    //       isValid: true,
-    //     }
-    //   }
-    // }));
-    // console.log(yo);
-
-    // const input = getByLabelText('Email Address');
-    // console.log(input);
-    // app.setState()
-
-//  })
-
-  // test('All inputs exist on default render', () => {
-  //   expect(getElementById('name').value).toEqual('');
-  //   expect(getElementById('emailAddress').value).toEqual('');
-  //   expect(getElementById('password').value).toEqual('');
-  
-  // })
-
-  // const linkElement = getByText(/learn react/i);
-  // const getById = queryByAttribute.bind(null, 'id');
-
-  // function checkElementIsInput (id) {
-  //   return  queryByAttribute('id',app.container, id).tagName === 'INPUT';
-  // }
-  //expect(app.getElementById('name')).toBeInTheDocument();
-
-  // attribute: string,
-  // container: HTMLElement,
-  // id: Matcher,
-  // options?: MatcherOptions,
-  // queryByAttribute(app.container, 'id','name')
-  // console.log(app.container);
-  // let el =).tagName;
-  // console.log(el);
-
-  
-  // expect(app.getElementById('emailAddress')).toBeInTheDocument();
-  // expect(app.getElementById('password')).toBeInTheDocument();
---
+++ src/SignupForm.test.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import { render, queryByAttribute, } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { fireEvent, getByText } from '@testing-library/dom';
+import SignupForm from './SignupForm';
+
+let component; 
+let getElementById = (id, container) => {
+  return queryByAttribute('id', container, id);
+}
+
+describe('Input Fields', () => {
+  function checkInitialState (inputEl) {
+    expect(inputEl.tagName).toEqual('INPUT');
+    expect(inputEl.className).toEqual('untouched');
+  }
+  
+  beforeEach(() => {
+    component = render(<SignupForm />);
+  });
+
+  test('name input field ', () => {
+    const nameInput = getElementById('name', component.container);
+
+    // 'name' -- initial state 
+    expect(nameInput.type).toEqual('text');
+    checkInitialState(nameInput);
+
+    // -- onChange to valid
+    fireEvent.change(nameInput, { target: { value: 'Parker' } })
+    expect(nameInput.className).toEqual('valid');
+
+    // -- onChange to invalid
+    fireEvent.change(nameInput, { target: { value: '' } })
+    expect(nameInput.className).toEqual('inValid');
+
+  }),
+
+  test('emailAddress input field ', () => {
+    const emailAddressInput = getElementById('emailAddress', component.container);
+
+    // 'emailAddress' -- initial state 
+    expect(emailAddressInput.type).toEqual('text');
+    checkInitialState(emailAddressInput);
+    
+    // -- onChange to valid
+    fireEvent.change(emailAddressInput, { target: { value: 'abc@def123.com' } });
+    expect(emailAddressInput.className).toEqual('valid'); 
+
+    // -- onChange to invalid
+    fireEvent.change(emailAddressInput, { target: { value: '' } });
+    expect(emailAddressInput.className).toEqual('inValid');
+  
+    fireEvent.change(emailAddressInput, { target: { value: 'abcdef123com' } });
+    expect(emailAddressInput.className).toEqual('inValid');
+
+  }),
+
+  test('password input field', () => {
+    const passwordInput = getElementById('password', component.container);
+    
+    // 'password' ---  initial state
+    expect(passwordInput.type).toEqual('password');
+    checkInitialState(passwordInput);
+    
+    // -- onChange to valid
+    fireEvent.change(passwordInput, { target: { value: 'aBcEf1234%$&*' } });
+    expect(passwordInput.className).toEqual('valid'); 
+    
+    // -- onChange to invalid
+    fireEvent.change(passwordInput, { target: { value: '' } });
+    expect(passwordInput.className).toEqual('inValid');
+  })
+});
+
+describe('Sign Up Button', () => {
+  test('is disabled when fields are invalid',()=>{
+    component = render(<SignupForm />);
+    const submitButton = getByText(component.container, 'Sign Up');
+    expect(submitButton.disabled).toBeTruthy();
+  }),
+
+  test('is enabled when fields are valid',()=>{
+    component = render(<SignupForm />);
+    const nameInput = getElementById('name', component.container);
+    const emailInput = getElementById('emailAddress', component.container);
+    const passwordInput = getElementById('password', component.container);
+    const submitButton = getByText(component.container, 'Sign Up');
+
+    fireEvent.change(
+      nameInput,
+      { target: { value: 'Parker' } }
+    );
+    fireEvent.change(
+      emailInput, 
+      { target: { value: 'abc@def123.com' } }
+    );
+    fireEvent.change(
+      passwordInput,
+      { target: { value: 'aBcEf1234%$&*' } }
+    );
+    expect(submitButton.disabled).toBeFalsy();
+  })
+});
