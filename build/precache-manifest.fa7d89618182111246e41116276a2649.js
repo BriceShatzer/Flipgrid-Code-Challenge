98447650ea5bba414923bd60caba091ef39da898
Brice Shatzer <brice.shatzer@gmail.com>
2020-05-19 05:57:24 -0500

    update to build

---
+++ build/precache-manifest.fa7d89618182111246e41116276a2649.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d947e2a74fd681811c107b0481dc5e51",
+    "url": "/Flipgrid-Code-Challenge/index.html"
+  },
+  {
+    "revision": "058ba41dd5bc8a81d346",
+    "url": "/Flipgrid-Code-Challenge/static/css/main.0c2269e5.chunk.css"
+  },
+  {
+    "revision": "9a0956500e10a6f66bed",
+    "url": "/Flipgrid-Code-Challenge/static/js/2.ea0359de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Flipgrid-Code-Challenge/static/js/2.ea0359de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "058ba41dd5bc8a81d346",
+    "url": "/Flipgrid-Code-Challenge/static/js/main.3190df45.chunk.js"
+  },
+  {
+    "revision": "b073c60c73f1dca7233d",
+    "url": "/Flipgrid-Code-Challenge/static/js/runtime-main.9a21e8fb.js"
+  },
+  {
+    "revision": "0f2d8b34f40d1d369505358047362630",
+    "url": "/Flipgrid-Code-Challenge/static/media/reset.0f2d8b34.svg"
+  }
+]);
